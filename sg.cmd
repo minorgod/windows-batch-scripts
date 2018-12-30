ca6957644b92313cafb3be3363bae2cbfa34923e
Brett Brewer <brett@brettbrewer.com>
2018-12-29 22:53:30 -0500

    Updated sg command

--- sg.cmd
+++ sg.cmd
@@ -1,10 +1,13 @@
 
 @SET param="%1"
 @SET currentDir=%~dp0
-@SET subgitDir="C:\"
-@SET sharedDaemonDir="%USERPROFILE%\scoop\apps\subgit\current\daemon-shared"
+@SET subgitDir=""%USERPROFILE%\scoop\apps\subgit\current"
+@SET sharedDaemonDir="%USERPROFILE%\scoop\persist\subgit\daemon-shared"
 @IF %param%=="start" (
   subgit daemon start %sharedDaemonDir%
-) ELSE (
+) ELSE IF %param%=="stop" (
   subgit daemon stop %sharedDaemonDir%
+) ELSE (
+  subgit --version
+  scoop which subgit
 )
